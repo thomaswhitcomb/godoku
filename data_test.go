67088f1e463308a5b5ec8aa698708835fc7a6731
twhitcomb <tom_whitcomb@intuit.com>
2018-01-23 11:19:51 -0800

    refactored testing code

--- data_test.go
+++ data_test.go
@@ -10,13 +10,28 @@ func TestCells(t *testing.T) {
 }
 
 func TestCross(t *testing.T) {
-	var unit *UnitType
-	unit = Cross("a", "b")
-	if (*unit)["ab"] != true {
-		t.Error("TestCross - expected ab")
+	tests := []struct {
+		name    string
+		parm1   string
+		parm2   string
+		size    int
+		indexes []string
+	}{
+		{"1", "a", "b", 1, []string{"ab"}},
+		{"2", "abc", "123", 9, []string{"a1", "a2", "c3", "b1", "b2", "b3", "c1", "c2", "c3"}},
 	}
-	unit = Cross("abc", "123")
-	if (*unit)["a1"] != true || (*unit)["a2"] != true || (*unit)["c3"] != true {
-		t.Error("TestCross - expected ab")
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			unit := Cross(tt.parm1, tt.parm2)
+			for _, x := range tt.indexes {
+				if len(*unit) != tt.size {
+					t.Errorf("bad size. expected %d and got %d", tt.size, len(*unit))
+				}
+				if (*unit)[x] != true {
+					t.Errorf("index %s should be true and it is not", x)
+				}
+			}
+		})
 	}
 }
